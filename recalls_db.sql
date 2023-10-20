0cda170a2fa2e4e908fe45e9313b536ed1746abd
Hayk Petrosyan <2126464@edu.vaniercollege.qc.ca>
2023-10-20 14:48:36 -0400

    changed purchase date in cars from int to date

--- recalls_db.sql
+++ recalls_db.sql
@@ -31,7 +31,7 @@ CREATE TABLE `cars` (
   `car_id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
   `model` int(11) NOT NULL,
-  `purchase_date` int(11) NOT NULL,
+  `purchase_date` date NOT NULL,
   `dealership` varchar(50) NOT NULL,
   `insurance_nb` varchar(9) NOT NULL,
   `mileage` int(11) NOT NULL,
