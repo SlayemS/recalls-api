9d27138f3a6c19a00168d833d3ced24b745e5e88
zehayk <hayk.minecraft@gmail.com>
2023-10-22 16:00:35 -0400

    forgot to remove some stuff from cars controller

--- app/Controllers/CarsController.php
+++ app/Controllers/CarsController.php
@@ -12,22 +12,6 @@ class CarsController extends BaseController
 {
     private $cars_model = null;
 
-    private $rules_create = array(
-        'first_name' => [
-            'required',
-            ['lengthMin', 1],
-            ['lengthMax', 45]
-        ],
-        'last_name' => [
-            'required',
-            ['lengthMin', 1],
-            ['lengthMax', 45]
-        ],
-        'last_update' => [
-            ['date', 'Y-m-d H:i:s']
-        ],
-    );
-
     function __construct() {
         $this->cars_model = new CarsModel();
     }
@@ -55,59 +39,4 @@ class CarsController extends BaseController
         return $response;
     }
 
-
-    public function handleGetFilmsByActorId(Request $request, Response $response, array $uri_args)
-    {
-        $film = $this->actors_model->getFilmsByActorId($uri_args);
-        
-        $film_json = json_encode($film);
-        
-        $response->getBody()->write($film_json);
-        return $response;
-    }
-
-    public function handleCreateActors(Request $request, Response $response)
-    {
-        $actors_data = $request->getParsedBody();
-
-        if (empty($actors_data) || !isset($actors_data)) {
-            throw new HttpInvalidInputException($request,
-            "Couldn't process the request... the list of films was empty!");
-        }
-
-
-        foreach ($actors_data as $key => $actor) {
-
-            $validation_response = $this->isValidData($actor, $this->rules_create);
-            if ($validation_response === true) {
-                $actor_id = $this->actors_model->createActor($actor);
-                $response->getBody()->write(json_encode(['actor_id' => $actor_id]));
-                return $response->withStatus(201);
-            }
-            else {
-
-                $response_data = array(
-                    "code" => 422,
-                    "message" => $validation_response
-                );
-        
-                return $this->prepareOkResponse(
-                    $response,
-                    $response_data,
-                    HttpCodes::STATUS_UNPROCESSABLE_ENTITY
-                );
-            }
-        }
-
-        $response_data = array(
-            "code" => HttpCodes::STATUS_CREATED,
-            "message" => "The list of actors has been successfully created"
-        );
-
-        return $this->prepareOkResponse(
-            $response,
-            $response_data,
-            HttpCodes::STATUS_CREATED
-        );
-    }
 }
