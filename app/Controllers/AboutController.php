5540b417ef502c85933149ed03cfe7a6aa6254ae
Phuc Nguyen <ndtphuccc@gmail.com>
2023-12-05 20:36:00 -0500

    update AboutController

--- app/Controllers/AboutController.php
+++ app/Controllers/AboutController.php
@@ -21,6 +21,13 @@ class AboutController extends BaseController
                 'instances' => 'Get all instances',
                 'instances/{instance_id}/repairs' => 'Get all repairs for a specific instance',
                 'cars/{car_id}/instance' => 'Get a specific instance for a specific car',
+                'manufacturers' => 'Filters: country, city, year',
+                'models' => 'Filters: year, vehicle_type, fuel_type, transmission_type, engine, power_type',
+                'recalls' => 'Filters: subject, issue_date, component',
+                'repairs' => 'Filters: status, max_cost, min_cost',
+                'customers' => 'Filters: first_name, last_name, customer_id',
+                'cars' => 'Filters: dealership, color, max_mileage, min_mileage, customer_id',
+                'instances' => 'Filters: job_done',
             ),
         );
         return $this->prepareOkResponse($response, $data);
