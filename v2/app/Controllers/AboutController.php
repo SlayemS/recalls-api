c5f62f7135ae3fe927a892ca6f0db53743c9ea07
Sayem <83617577+SlayemS@users.noreply.github.com>
2023-12-07 17:46:14 -0500

    Copied v1 AboutController.php to v2 due to versioning issues

--- v2/app/Controllers/AboutController.php
+++ v2/app/Controllers/AboutController.php
@@ -25,13 +25,13 @@ class AboutController extends BaseController
         $data = array(
             'about' => 'Welcome, this is a Web service that provides information about car manufacturers, models, recalls, repairs, and customers.',
             'resources' => array(
-                'manufacturers' => 'Get all manufacturers',
-                'models' => 'Get all models',
-                'recalls' => 'Get all recalls',
-                'repairs' => 'Get all repairs',
-                'customers' => 'Get all customers',
-                'cars' => 'Get all cars',
-                'instances' => 'Get all instances',
+                'manufacturers' => ['Description' => 'Get all manufacturers','Filters' => 'year, vehicle_type, fuel_type, transmission_type, engine, power_type' ],
+                'models' => ['Description' => 'Get all models', 'Filters' => 'year, vehicle_type, fuel_type, transmission_type, engine, power_type' ],
+                'recalls' => ['Description' => 'Get all recalls','Filters' => 'subject, issue_date, component' ],
+                'repairs' => ['Description' => 'Get all repairs', 'Filters' => 'status, max_cost, min_cost'],
+                'customers' => ['Description' => 'Get all customers', 'Filters' => 'first_name, last_name, customer_id'],
+                'cars' => ['Description' => 'Get all cars', 'Filters' => 'dealership, color, max_mileage, min_mileage, customer_id'],
+                'instances' => ['Description' => 'Get all instances', 'Filters' => 'job_done'],
                 'instances/{instance_id}/repairs' => 'Get all repairs for a specific instance',
                 'cars/{car_id}/instance' => 'Get a specific instance for a specific car'
             ),
